fa15c4684df7eb6a1390f4ffce8ad6259b779fdc
Nikhil Kumar <42882230+quietNix@users.noreply.github.com>
2022-05-09 08:10:57 +0530

    Deploy heroku

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const Gun = require('gun');
+const cors = require("cors")
 const app = express()
 const port = 3030
 app.use(Gun.serve);
+
+app.use(cors());
 
 const server = app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
